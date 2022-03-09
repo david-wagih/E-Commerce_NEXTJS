8400ea202e3029ff7956cd5707776e35e684b543
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-03-09 19:27:45 +0200

    defining the layout for the home page

--- amazon/pages/index.js
+++ amazon/pages/index.js
@@ -1,16 +1,5 @@
-import { Typography } from "@mui/material";
-import Head from "next/head";
-import Image from "next/image";
+import Layout from "../components/Layout";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Amazon</title>
-        <meta name="description" content="E-Commerce Website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Typography>Hello from Amazon App</Typography>
-    </div>
-  );
+  return <Layout>List Products</Layout>;
 }
